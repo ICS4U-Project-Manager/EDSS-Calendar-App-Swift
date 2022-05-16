fb0006237b39ee0ae058ca6094428d79c81c6b57
ICS4U-Project-Manager <ics4uclass2022@gmail.com>
2022-05-16 09:38:36 -0400

    finished navigation skeleton

---
+++ Calendar App/SigninViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SigninViewController.swift
+//  Calendar App
+//
+//  Created by Zack Liam and Lucas on 2022-05-13.
+//
+
+import UIKit
+
+class SigninViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signOutUnwind(_ sender: UIStoryboardSegue) {}
+}
