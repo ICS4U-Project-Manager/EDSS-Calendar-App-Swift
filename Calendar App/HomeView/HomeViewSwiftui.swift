5395266edd6107fb57d2fd970bd849ad1cf15f0d
Sonia Snyder <soniaeesnyder@gmail.com>
2022-05-27 09:59:18 -0400

    Revert "Merge branch 'main' of https://github.com/ICS4U-Project-Manager/EDSS-Calendar-App-Swift"
    This reverts commit 2f8ff37f036f98e3fcecc59bc92a42cd9dd810b1, reversing
    changes made to 104e02c931c78a92c7495475392a57c235043fe8.

--- Calendar App/HomeView/HomeViewSwiftui.swift
+++ Calendar App/HomeView/HomeViewSwiftui.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Kyle Stirbet on 2022-05-16.
 //
-
 import SwiftUI
 
 struct HomeViewSwiftUI: View {
@@ -12,17 +11,18 @@ struct HomeViewSwiftUI: View {
     var body: some View {
         VStack {
             ForEach(viewModel.users) { user in
-                
                 Text("Today")
                     .fontWeight(.bold)
                     .position(x: 100, y: 15)
-                    .font(.system(size: 70))
+                    .font(.system(size: 60))
+                    .font(.title)
                     .foregroundColor(Color("Off white"))
                 
+                
                 Rectangle()
-                    .background(Color.red)
                     .frame(width: 350, height: 5)
                     .position(x: 187, y: -35)
+                
                 
                 Text("Announcements")
                     .fontWeight(.black)
@@ -30,33 +30,33 @@ struct HomeViewSwiftUI: View {
                     .font(.title2)
                     .position(x: 100, y: -95)
                 
+                
                 VStack{
-                    Text(user.Title)
+                Text(user.Title)
                         .fontWeight(.bold)
                         .font(.system(size: 30))
                         .font(.title)
-                    Text(user.Lines)
+                Text(user.Title)
                         .fontWeight(.bold)
                         .font(.system(size: 30))
                         .font(.title)
                 }
-                .background(
-                    Image("Text Boxes")
-                        .resizable()
-                        .frame(width: 311, height: 136))
-                .frame(width: 311, height: 136)
-                .foregroundColor(Color("Dark green"))
-                .position(x: 187, y: -100)
-                .gesture(DragGesture(minimumDistance: 3.0, coordinateSpace: .local)
-                    .onEnded { value in
-                        print(value.translation)
-                        switch(value.translation.width, value.translation.height) {
-                        case (-100...100, ...0):  print("up swipe"); viewModel.num += 1; print (viewModel.num); self.viewModel.fetchData()
-                        case (-100...100, 0...):  print("down swipe"); viewModel.num -= 1; print (viewModel.num); self.viewModel.fetchData()
-                        default:  print("no clue")
+                    .frame(width: 365, height: 175)
+                    .background(Color("Off white"))
+                    .cornerRadius(25)
+                    .foregroundColor(Color("Dark green"))
+                    .position(x: 187, y: -80)
+                    .gesture(DragGesture(minimumDistance: 3.0, coordinateSpace: .local)
+                        .onEnded { value in
+                            print(value.translation)
+                            switch(value.translation.width, value.translation.height) {
+                            case (-100...100, ...0):  print("up swipe"); viewModel.num += 1; print (viewModel.num); self.viewModel.fetchData()
+                                case (-100...100, 0...):  print("down swipe"); viewModel.num -= 1; print (viewModel.num); self.viewModel.fetchData()
+                                default:  print("no clue")
+                            }
                         }
-                    }
-                )
+                    )
+                
                 
                 Text("Clubs & Groups")
                     .fontWeight(.black)
@@ -64,27 +64,16 @@ struct HomeViewSwiftUI: View {
                     .font(.title2)
                     .position(x: 100, y: -60)
                 
-                Text("nothing to see here")
+                
+                Text("Come Back Some Time Else. Nothing Yet.")
                     .fontWeight(.bold)
-                    .font(.system(size: 30))
-                    .font(.title)
-                    .background(
-                        Image("Text Boxes")
-                            .resizable()
-                            .frame(width: 311, height: 136))
-                    .frame(width: 311, height: 136)
                     .foregroundColor(Color("Dark green"))
-                    .position(x: 187, y: -80)
-                    .gesture(DragGesture(minimumDistance: 3.0, coordinateSpace: .local)
-                        .onEnded { value in
-                            print(value.translation)
-                            switch(value.translation.width, value.translation.height) {
-                            case (-100...100, ...0):  print("up swipe")
-                            case (-100...100, 0...):  print("down swipe")
-                            default:  print("no clue")
-                            }
-                        }
-                    )
+                    .font(.largeTitle)
+                    .frame(alignment: .center)
+                    .background(Color("Off white"))
+                    .cornerRadius(25)
+                    .frame(width: 365, height: 175, alignment: .center)
+                    .position(x: 187, y: -45)
                 
                 Link("🔗EDSS Instagram",
                      destination: URL(string: "https://www.instagram.com/elmira_lancers/")!)
@@ -93,8 +82,6 @@ struct HomeViewSwiftUI: View {
                 .cornerRadius(20)
                 .foregroundColor(Color("Dark green"))
                 .position(x: 187, y:-15)
-                
-                
             }
         }
         .onAppear(){self.viewModel.fetchData()
@@ -103,6 +90,7 @@ struct HomeViewSwiftUI: View {
         .background(Color("Dark green"))
     }
 }
+
 
 
 struct HomeViewSwiftUI_Previews: PreviewProvider {
@@ -114,3 +102,4 @@ struct HomeViewSwiftUI_Previews: PreviewProvider {
         }
     }
 }
+
