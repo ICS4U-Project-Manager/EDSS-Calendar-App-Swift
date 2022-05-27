993ade749f90afd6358fc5c1c17e37260877ac6c
Sonia Snyder <soniaeesnyder@gmail.com>
2022-05-27 09:59:28 -0400

    Revert "made 3 text boxes and changed cancel button for add event page, add holden did colours"
    This reverts commit 104e02c931c78a92c7495475392a57c235043fe8.

--- Calendar App/AddEventViewController.swift
+++ Calendar App/AddEventViewController.swift
@@ -7,54 +7,14 @@
 
 import UIKit
 
-class AddEventViewController: UIViewController, UITextFieldDelegate {
+class AddEventViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        cancelButton.layer.borderWidth = 1
-        cancelButton.layer.borderColor = CGColor(red: 113/255, green: 92/255, blue: 131/255, alpha: 1)
-        //preparing text field to be able to exit keyboard
-        self.titleTextField.delegate = self
     }
-    //OUTLETS
-    @IBOutlet weak var titleTextField: UITextField!
-    @IBOutlet weak var locationTextField: UITextField!
-    @IBOutlet weak var descriptionTextField: UITextField!
-    @IBOutlet weak var cancelButton: UIButton!
-
-    //KEYBOARD FUNCTIONS
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        return titleTextField.resignFirstResponder()
-    }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
-    
-    //CLASS PROPERTY VARIABLES
-    var nameOfEvent : String = ""
-    //var allDayEvent : Bool
-    //var startTime
-    //var endTime
-    var locationOfEvent : String = ""
-    var descriptionOfEvent : String = ""
-    //var clubOrGroup : String
-    
-    //ASSIGNING VALUES
-    //title
-    @IBAction func assignEventName(_ sender: Any) {
-        nameOfEvent = titleTextField.text!
-    }
-    //location
-    @IBAction func assignEventLocation(_ sender: Any) {
-        locationOfEvent = locationTextField.text!
-    }
-    //description
-    @IBAction func assignEventDescription(_ sender: Any) {
-        descriptionOfEvent = descriptionTextField.text!
-    }
-    
+   
 }
 
 
