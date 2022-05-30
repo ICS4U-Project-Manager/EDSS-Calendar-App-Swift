ed21b02e452460315fb2335a8f7eff5ce8d1a1bb
ICS4U-Project-Manager <ics4uclass2022@gmail.com>
2022-05-30 10:09:31 -0400

    3 text boxes for location description and title that read and assign to local variable and will later be assigned to event class

--- Calendar App/AddEventViewController.swift
+++ Calendar App/AddEventViewController.swift
@@ -7,14 +7,59 @@
 
 import UIKit
 
-class AddEventViewController: UIViewController {
+class AddEventViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    
+        //Preparing keyboard exit
+        self.titleTextField.delegate = self
     }
-   
+    
+    //Keyboard Dissapear Functions
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if whichTextBox == 1 {
+        return titleTextField.resignFirstResponder()
+        }
+        else if whichTextBox == 2 {
+           return locationTextField.resignFirstResponder()
+        }
+        else if whichTextBox == 3 {
+            return descriptionTextField.resignFirstResponder()
+        }
+        return false
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    //OUTLETS and VARIABLES
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    var whichTextBox : Int = 0
+    var eventTitle : String = ""
+    var eventLocation : String = ""
+    var eventDescription : String = ""
+    
+    //functions
+    @IBAction func assignEventTitle(_ sender: Any) {
+        eventTitle = titleTextField.text!
+        whichTextBox = 1
+    }
+    @IBAction func assignEventLocation(_ sender: Any) {
+        eventLocation = locationTextField.text!
+        whichTextBox = 2
+    }
+    @IBAction func assignEventDescription(_ sender: Any) {
+        eventDescription = descriptionTextField.text!
+        whichTextBox = 3
+    }
+    
+    
 }
 
 
