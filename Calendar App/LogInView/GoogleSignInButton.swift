35aaa6058aa122175aa9bce2d944615f76f63022
ICS4U-Project-Manager <104844300+ICS4U-Project-Manager@users.noreply.github.com>
2022-06-01 10:42:12 -0400

    firebase stuff

---
+++ Calendar App/LogInView/GoogleSignInButton.swift
@@ -0,0 +1,25 @@
+//
+//  GoogleSignInButton.swift
+//  test55
+//
+//  Created by Kyle Stirbet on 2022-05-13.
+//
+
+import Foundation
+import SwiftUI
+import GoogleSignIn
+
+struct GoogleSignInButton: UIViewRepresentable {
+  @Environment(\.colorScheme) var colorScheme
+  
+  private var button = GIDSignInButton()
+
+  func makeUIView(context: Context) -> GIDSignInButton {
+    button.colorScheme = colorScheme == .dark ? .dark : .light
+    return button
+  }
+
+  func updateUIView(_ uiView: UIViewType, context: Context) {
+    button.colorScheme = colorScheme == .dark ? .dark : .light
+  }
+}
