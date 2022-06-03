df9e576d084328075b5e2600d697acb6b7757a44
ICS4U-Project-Manager <ics4uclass2022@gmail.com>
2022-06-03 10:37:58 -0400

    Formatted Filter Viewcontroller and added button outlets for it

--- Calendar App/FilterViewController.swift
+++ Calendar App/FilterViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class FilterViewController: UIViewController {
 
+    @IBOutlet weak var exitPage: UIButton!
+    @IBOutlet weak var applyButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //Prepare button visuals
+        exitPage.layer.borderWidth = 2
+        exitPage.layer.borderColor = CGColor(red: 165/255, green: 127/255, blue: 196/255, alpha: 1)
+        exitPage.layer.cornerRadius = 10
+        
+        applyButton.layer.borderWidth = 4
+        applyButton.layer.borderColor = CGColor(red: 113/255, green: 92/255, blue: 131/255, alpha: 1)
+        applyButton.layer.cornerRadius = 10
+        
         // Do any additional setup after loading the view.
     }
     
