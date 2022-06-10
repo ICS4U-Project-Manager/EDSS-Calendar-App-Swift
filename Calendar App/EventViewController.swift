c673828226f0326e8a4481940cc9d3e92525d468
Sonia Snyder <soniaeesnyder@gmail.com>
2022-06-10 10:45:00 -0400

    Revert "Event View Display Fire Store"
    This reverts commit 62142ae59192fcf172c1c04d5d0319be048b8f78.

--- Calendar App/EventViewController.swift
+++ Calendar App/EventViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Zack Liam and Lucas on 2022-05-16.
 //
-
 import UIKit
 import Firebase
 import FirebaseFirestore
@@ -15,31 +14,11 @@ class EventViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
     
-    var db = Firestore.firestore()
-    var users = [User]()
+    var viewModel = userViewModel()
    
     
     override func viewDidLoad() {
-        
-        
-        self.db.collection("users").whereField("idd", isEqualTo: 1).addSnapshotListener { (querySnapshot, error) in
-        guard let documents = querySnapshot?.documents else {
-            print("No documents")
-            return
-            }
-            self.users = documents.map { (queryDocumentSnapshot) -> User in
-                let data = queryDocumentSnapshot.data()
-                let Title = data["Title"] as? String ?? ""
-                let Body = data["Body"] as? String ?? ""
-                let Lines = data["Lines"] as? String ?? ""
-                let idd = data["idd"] as? Int ?? 0
-                
-                self.label.text = Title
-                
-                return User(Title: Title, Body: Body, Lines: Lines, idd: idd)
-            }
-        }
+        label.text = "user.Title"
     }
 }
-
 
