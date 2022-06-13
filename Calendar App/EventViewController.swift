4a622d351db87a262d696a82dce8472aaf10c425
ICS4U-Project-Manager <104844300+ICS4U-Project-Manager@users.noreply.github.com>
2022-06-13 09:28:52 -0400

    Revert "Event View Controller"
    This reverts commit af095704f654b235b8d877bcf50cc09e28f08230.

--- Calendar App/EventViewController.swift
+++ Calendar App/EventViewController.swift
@@ -11,70 +11,22 @@ import FirebaseCore
 import FirebaseStorage
 
 class EventViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
     
-    @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var startDate: UILabel!
-    @IBOutlet weak var endDate: UILabel!
-    @IBOutlet weak var startTime: UILabel!
-    @IBOutlet weak var endTime: UILabel!
-    @IBOutlet weak var location: UILabel!
-    @IBOutlet weak var descript: UILabel!
-    @IBOutlet weak var group: UILabel!
-    
-    
-    var db = Firestore.firestore()
-    var events = [event]()
+    var viewModel = userViewModel()
+    var event : ICSEvent?
     
     override func viewDidLoad() {
-        
-        self.db.collection("Events").getDocuments() { [self] (querySnapshot, error) in
-            guard let documents2 = querySnapshot?.documents else {
-                print("No documents")
-                return
-            }
-            self.events = documents2.map { (queryDocumentSnapshot) -> event in
-                let data2 = queryDocumentSnapshot.data()
-                let name=data2["name"]! as! String
-                let description=data2["description"]! as? String ?? ""
-                let location=data2["location"]! as? String ?? ""
-                let group=data2["group"]! as? String ?? ""
-                let idd=data2["idd"]! as? Int ?? 0
-                let startDate = (data2["startDate"] as? Timestamp)?.dateValue() ?? Date()
-                var startDateF: String {
-                    let formatter = DateFormatter()
-                    formatter.dateFormat = "MMM d, yyyy"
-                    return formatter.string(from: startDate)
-                }
-                var startTimeF: String {
-                    let formatter = DateFormatter()
-                    formatter.dateFormat = "hh:mm"
-                    return formatter.string(from: startDate)
-                }
-                
-                
-                print(idd)
-                print(startDateF)
-                
-                self.name.text = name
-                self.startDate.text = startDateF
-                self.startTime.text = startTimeF
-                self.descript.text = description
-                
-                
-                
-                return event(name: name, description: description, location: location, group: group, idd: idd, startDate: startDate)
-            }
+        label.text = "user.Title"
+        if let label:String = event?.getName() {
+        classLabel.text = label
+        }
+        else {
+            classLabel.text = "event not found"
         }
     }
+    @IBOutlet weak var classLabel: UILabel!
+    
 }
 
-
-struct event: Identifiable {
-    var id: String = UUID().uuidString
-    var name: String
-    var description: String
-    var location: String
-    var group: String
-    var idd: Int
-    var startDate: Date
-}
