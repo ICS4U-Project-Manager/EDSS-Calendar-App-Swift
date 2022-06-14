4506906b4776981e287869d8d12a1a379a375214
ICS4U-Project-Manager <ics4uclass2022@gmail.com>
2022-06-14 10:44:38 -0400

    updated classes and helper functions

--- Calendar App/Calendar App Classes Documentation.swift
+++ Calendar App/Calendar App Classes Documentation.swift
@@ -7,6 +7,7 @@
  ICSEvent
  
  Class which represents an event on the calendar
+ 
  init(name:String, description:String, location:String, group:String, startDate:ICSDate, endDate:ICSDate, startTime:ICSTime, endTime:ICSTime, id:Int) (group is optional)
  
  Properties:
@@ -18,17 +19,20 @@
  
  setName(_ name:String)
      Sets the name of the event.
+ 
  All other getters and setters follow the same format. Also, getGroup returns an empty string if the group is not provided.
  
  getDuration() -> ICSTime
      Returns the computed duration of the event. Duration has no setter.
  Note that the returned duration is an ICSTime object with the "isCyclic" flag set to false (see ICSTime, below.)
- I
  
- CSDate
+ 
+ ICSDate
  
  Class which represents a date.
+ 
  init(year:Int,month:Int,day:Int)
+ 
  init(year:Int,month:String,day:Int) (Name of month must be the full name, and the first letter must be upper-case.)
  
  Properties:
@@ -49,7 +53,9 @@
  ICSTime
  
  Class which represents a time.
+ 
  init(hour:Int,minute:Int,period:Int) (The period must be either 0 or 1, 0 for AM and 1 for PM.)
+ 
  init(time:Int,isCyclic:Bool) (This initializer is used internally by the event class, you will almost certainly never need to use it. Just know that the "duration" of the ICSEvent class uses this initializer, and is not constrained by AM and PM.)
  
  Properties:
@@ -59,5 +65,17 @@
  getHour, setHour, getMinute, setMinute, getPeriod, and setPeriod : Same as above.
  
  getText() -> String : Returns the time in standard human-readable time format.
+ 
+ ICSEventSave(_ events:[ICSEvent]) -> [[String:Any]]
+ 
+ Returns a list of dictionaries representing the event objects in a format which can be stored as json.
+ 
+ ICSEventLoad(_ events:[[String:Any]]) -> [ICSEvent]
+ 
+ Converts a list of dictionaries (retrieved from a json file) and converts it into a list of ICSEvent objects for use in the app.
+ 
+ ICSFilterEvents(groups:[String],events:[ICSEvent]) -> [ICSEvent]
+ 
+ Allows the filtering of the event list by group. Returns a list of events associated with any of the given groups.
  
  */
---
+++ Calendar App/Data Helper Functions.swift
@@ -0,0 +1,70 @@
+//Calendar app data helper functions
+//Ethan Warren
+//06/07/2022
+//ICS4UI
+
+func ICSEventSave(_ events:[ICSEvent]) -> [[String:Any]] {
+    var result:[[String:Any]]=[]
+    for event in events {
+        let eventDict:[String:Any]
+        let name=event.getName()
+        let description=event.getDescription()
+        let location=event.getLocation()
+        let group=event.getGroup()
+        let id=event.getId()
+        let startYear=event.getStartDate().getYear()
+        let startMonth=event.getStartDate().getMonth()
+        let startDay=event.getStartDate().getDay()
+        let endYear=event.getEndDate().getYear()
+        let endMonth=event.getEndDate().getMonth()
+        let endDay=event.getEndDate().getDay()
+        let startHour=event.getStartTime().getHour()
+        let startMinute=event.getStartTime().getMinute()
+        let startPeriod=event.getStartTime().getPeriod()
+        let endHour=event.getEndTime().getHour()
+        let endMinute=event.getEndTime().getMinute()
+        let endPeriod=event.getEndTime().getPeriod()
+        eventDict=["name":name,"description":description,"location":location,"group":group,"startYear":startYear,"startMonth":startMonth,"startDay":startDay,"endYear":endYear,"endMonth":endMonth,"endDay":endDay,"startHour":startHour,"startMinute":startMinute,"startPeriod":startPeriod,"endHour":endHour,"endMinute":endMinute,"endPeriod":endPeriod,"id":id]
+        result.append(eventDict)
+    }
+    return result
+}
+
+func ICSEventLoad(_ events:[[String:Any]]) -> [ICSEvent] {
+    var result:[ICSEvent]=[]
+    for event in events {
+        let name=event["name"]! as! String
+        let description=event["description"]! as! String
+        let location=event["location"]! as! String
+        let group=event["group"]! as! String
+        let id=event["id"]! as! Int
+        let startYear=event["startYear"]! as! Int
+        let startMonth=event["startMonth"]! as! Int
+        let startDay=event["startDay"]! as! Int
+        let endYear=event["endYear"]! as! Int
+        let endMonth=event["endMonth"]! as! Int
+        let endDay=event["endDay"]! as! Int
+        let startHour=event["startHour"]! as! Int
+        let startMinute=event["startMinute"]! as! Int
+        let startPeriod=event["startPeriod"]! as! Int
+        let endHour=event["endHour"]! as! Int
+        let endMinute=event["endMinute"]! as! Int
+        let endPeriod=event["endPeriod"]! as! Int
+        let startDate:ICSDate=ICSDate(year:startYear,month:startMonth,day:startDay)
+        let endDate:ICSDate=ICSDate(year:endYear,month:endMonth,day:endDay)
+        let startTime:ICSTime=ICSTime(hour:startHour,minute:startMinute,period:startPeriod)
+        let endTime:ICSTime=ICSTime(hour:endHour,minute:endMinute,period:endPeriod)
+        result.append(ICSEvent(name:name,description:description,location:location,group:group,startDate:startDate,endDate:endDate,startTime:startTime,endTime:endTime,id:id))
+    }
+    return result
+}
+
+func ICSFilterEvents(groups:[String],events:[ICSEvent]) -> [ICSEvent] {
+    var result:[ICSEvent]=[]
+    for event in events {
+        if groups.contains(event.getGroup()) {
+            result.append(event)
+        }
+    }
+    return result
+}
